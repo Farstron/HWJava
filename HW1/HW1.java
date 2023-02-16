ba003b2045dc2a411e1ce447968e61966f704991
Farstron <109409135+Farstron@users.noreply.github.com>
2023-02-16 16:12:28 +0700

    n_triangle add

--- HW1/HW1.java
+++ HW1/HW1.java
@@ -6,4 +6,8 @@ public class HW1 {
     public static void main(String[] args) {
         
     }
+    public static int n_triangle(int n) {
+        int res = (n*(n+1))/2;
+        return res;
+    }
 }
