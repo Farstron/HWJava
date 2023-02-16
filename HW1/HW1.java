b48741cd71b5386876f26981c488e7a696716ace
Farstron <109409135+Farstron@users.noreply.github.com>
2023-02-16 16:13:13 +0700

    Read add

--- HW1/HW1.java
+++ HW1/HW1.java
@@ -1,5 +1,5 @@
 package HW1;
-
+import java.util.Scanner;
 // Написать программу вычисления n-ого треугольного числа.
 public class HW1 {
 
@@ -9,5 +9,14 @@ public class HW1 {
     public static int n_triangle(int n) {
         int res = (n*(n+1))/2;
         return res;
+    }
+    public static int Read (String way){
+        int n =0;
+        if (way == ""){
+            Scanner scan = new Scanner(System.in);
+            n = scan.nextInt();
+            scan.close();
+        }
+        return n;
     }
 }
