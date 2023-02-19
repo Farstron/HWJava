1361919181971fd2dcbb4910b592f93c0ecb8226
Farstron <109409135+Farstron@users.noreply.github.com>
2023-02-19 23:12:39 +0700

    arrayToString add (HW3)

--- HW3/HW3.java
+++ HW3/HW3.java
@@ -56,4 +56,17 @@ public class HW3 {
         }
         return currentSrc;
     }
+
+    private static String arrayToString(int[] array) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < array.length; i++) {
+            if (i > 0) {
+                sb.append(", ");
+            }
+            sb.append(array[i]);
+        }
+        sb.append("]");
+        return sb.toString();
+    }
 }
