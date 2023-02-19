2974d821f44debced233137028e39bf6c7a8e847
Farstron <109409135+Farstron@users.noreply.github.com>
2023-02-19 23:11:27 +0700

    merge add(HW3)

--- HW3/HW3.java
+++ HW3/HW3.java
@@ -6,4 +6,34 @@ public class HW3 {
     public static void main(String[] args) {
         
     }
+
+    private static void merge(int[] src1, int src1Start, int[] src2, int src2Start, int[] dest,
+                              int destStart, int size) {
+        int index1 = src1Start;
+        int index2 = src2Start;
+
+        int src1End = Math.min(src1Start + size, src1.length);
+        int src2End = Math.min(src2Start + size, src2.length);
+
+        if (src1Start + size > src1.length) {
+            for (int i = src1Start; i < src1End; i++) {
+                dest[i] = src1[i];
+            }
+            return;
+        }
+
+        int iterationCount = src1End - src1Start + src2End - src2Start;
+
+        for (int i = destStart; i < destStart + iterationCount; i++) {
+            if (index1 < src1End && (index2 >= src2End || src1[index1] < src2[index2])) {
+                dest[i] = src1[index1];
+                index1++;
+            } else {
+                dest[i] = src2[index2];
+                index2++;
+            }
+        }
+    }
+
+    
 }
