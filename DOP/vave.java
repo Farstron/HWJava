6dab2cb97a6f6c507754439645895240714a42fe
Farstron <109409135+Farstron@users.noreply.github.com>
2023-02-22 22:14:09 +0700

    refresh

--- DOP/vave.java
+++ DOP/vave.java
@@ -2,22 +2,36 @@ package DOP;
 
 import java.util.ArrayDeque;
 
+class Coordinates{
+    int X;
+    int Y;
+    Coordinates(int x, int y) {
+        this.X = x;
+        this.Y = y;
+    }
+}
+class MinCoordinates{
+    int min;
+    Coordinates co;
+    MinCoordinates(int min, Coordinates co) {
+        this.min = min;
+        this.co = co;
+    }
+}
 public class vave {
     
     public static void main(String[] args) {
         int[][] map = createMap(6,6);
-        // drawMap(map);
         addWall(4,3,2,1,map);
         addWall(5,6,2,0,map);
-        addEscape(7,4,map);
         ArrayDeque<Coordinates> Queue = new ArrayDeque<Coordinates>();
         Queue.add(addCat(2,2,map));
-        // System.out.println("_______________"+Queue.getFirst().X+","+Queue.getFirst().Y);
-        // Coordinates c = Queue.removeFirst();
-        // System.out.println("_______________"+c.X+","+c.Y);
-        // System.out.println("_______________"+Queue.getFirst().X+","+Queue.getFirst().Y);
-
+        drawMap(map);
         maping(map,Queue);
+        drawMap(map);
+        Queue.add(addEscape(7,4,map));
+        drawMap(map);
+        findWay(map, Queue);
         drawMap(map);
     }
     public static Coordinates addCat(int x,int y,int[][] map){
@@ -28,58 +42,54 @@ public class vave {
     public static void maping(int[][] map, ArrayDeque<Coordinates> qC){
         if (check(map,0)==true){
             Coordinates nP = qC.removeFirst();
-            System.out.println("_______________"+nP.X+","+nP.Y);
             goUp(nP, qC, map);
             goRight(nP, qC, map);
             goDown(nP, qC, map);
             goLeft(nP, qC, map);
-            System.out.println("_______________"+qC.getFirst().X+","+qC.getFirst().Y);
             maping(map,qC);
         }
     }
     public static void goUp(Coordinates pos, ArrayDeque<Coordinates> qC, int[][] map){
-        Coordinates tempu = pos;
-        System.out.println("goUp "+tempu.X+","+tempu.Y);
-        tempu.Y=tempu.Y+1;
-        System.out.println("goUp "+tempu.X+","+tempu.Y);
-        if (map[tempu.Y][tempu.X] == 0){
-            qC.add(tempu);
-            map[tempu.Y][tempu.X] = map[pos.Y][pos.X]+1;
+        Coordinates temp = new Coordinates(0, 0);
+        temp.X = pos.X;
+        temp.Y = pos.Y;
+        temp.Y=temp.Y+1;
+        if (map[temp.Y][temp.X] == 0){
+            qC.add(temp);
+            map[temp.Y][temp.X] = map[pos.Y][pos.X]+1;
         }
         
     }
     public static void goRight(Coordinates pos, ArrayDeque<Coordinates> qC, int[][] map){
-        Coordinates tempr = pos;
-        System.out.println("goRight "+tempr.X+","+tempr.Y);
-        tempr.X=tempr.X+1;
-        System.out.println("goRight "+tempr.X+","+tempr.Y);
-        if (map[tempr.Y][tempr.X] == 0){
-            qC.add(tempr);
-            map[tempr.Y][tempr.X] = map[pos.Y][pos.X]+1;
-        }
-    }
-    public static void goDown(Coordinates pos, ArrayDeque<Coordinates> qC, int[][] map){
-        Coordinates tempd = pos;
-        System.out.println("goDown "+tempd.X+","+tempd.Y);
-        tempd.Y=tempd.Y-1;
-        System.out.println("goDown "+tempd.X+","+tempd.Y);
-        if (map[tempd.Y][tempd.X] == 0){
-            qC.add(tempd);
-            map[tempd.Y][tempd.X] = map[pos.Y][pos.X]+1;
-        }
-    }
-    public static void goLeft(Coordinates pos, ArrayDeque<Coordinates> qC, int[][] map){
-        Coordinates temp = pos;
-        System.out.println("goLeft "+temp.X+","+temp.Y);
-        temp.X=temp.X-1;
-        System.out.println("goLeft "+temp.X+","+temp.Y);
+        Coordinates temp = new Coordinates(0, 0);
+        temp.X = pos.X;
+        temp.Y = pos.Y;
+        temp.X=temp.X+1;
         if (map[temp.Y][temp.X] == 0){
             qC.add(temp);
             map[temp.Y][temp.X] = map[pos.Y][pos.X]+1;
         }
     }
-    
-
+    public static void goDown(Coordinates pos, ArrayDeque<Coordinates> qC, int[][] map){
+        Coordinates temp = new Coordinates(0, 0);
+        temp.X = pos.X;
+        temp.Y = pos.Y;
+        temp.Y=temp.Y-1;
+        if (map[temp.Y][temp.X] == 0){
+            qC.add(temp);
+            map[temp.Y][temp.X] = map[pos.Y][pos.X]+1;
+        }
+    }
+    public static void goLeft(Coordinates pos, ArrayDeque<Coordinates> qC, int[][] map){
+        Coordinates temp = new Coordinates(0, 0);
+        temp.X = pos.X;
+        temp.Y = pos.Y;
+        temp.X=temp.X-1;
+        if (map[temp.Y][temp.X] == 0){
+            qC.add(temp);
+            map[temp.Y][temp.X] = map[pos.Y][pos.X]+1;
+        }
+    }
     private static boolean check(int[][] map, int toCheckValue)
     {
         boolean test = false;
@@ -93,11 +103,69 @@ public class vave {
         }
         return test;
     }
-    public static void findWay(int[][] map){
-
+    public static void findWay(int[][] map,ArrayDeque<Coordinates> qC){
+        if (map[qC.getFirst().Y+1][qC.getFirst().X] != 1 &&
+        map[qC.getFirst().Y-1][qC.getFirst().X] != 1 &&
+        map[qC.getFirst().Y][qC.getFirst().X+1] != 1 &&
+        map[qC.getFirst().Y][qC.getFirst().X-1] != 1){
+            System.out.println(qC.getFirst().X+","+qC.getFirst().Y);
+            findMin(map,qC);
+            System.out.println(qC.getFirst().X+","+qC.getFirst().Y);
+            map[qC.getFirst().Y][qC.getFirst().X]=0;
+            // findWay(map,qC);
+        }
+        map[qC.getFirst().Y][qC.getFirst().X]=0; 
+        System.out.println(qC.getFirst().X+","+qC.getFirst().Y);
     }
-    public static void addEscape(int x,int y,int[][] map){
+    public static void findMin(int[][] map, ArrayDeque<Coordinates> qC){
+        MinCoordinates mC = new MinCoordinates(map[qC.getFirst().Y][qC.getFirst().X], new Coordinates(qC.getFirst().X, qC.getFirst().Y));
+        while(qC.peek()!=null){
+            Coordinates temp = new Coordinates(0, 0);
+            temp.X = qC.getFirst().X;
+            temp.Y = qC.getFirst().Y;
+            qC.remove();
+            checkUp(temp,map,mC);
+            checkRight(temp,map,mC);
+            checkDown(temp,map,mC);
+            checkLeft(temp,map,mC);
+        }
+        
+        qC.add(mC.co);
+    }
+    public static void checkUp(Coordinates pos, int[][] map, MinCoordinates mC){
+        System.out.println(mC.min+"----"+map[pos.Y+1][pos.X]);
+        if (map[pos.Y+1][pos.X] < mC.min && map[pos.Y+1][pos.X]>0){
+            mC.min=map[pos.Y+1][pos.X];
+            mC.co.X=pos.X;
+            mC.co.Y=pos.Y+1;
+        }
+        
+    }
+    public static void checkRight(Coordinates pos, int[][] map, MinCoordinates mC){
+        if (map[pos.Y][pos.X+1] < mC.min && map[pos.Y][pos.X+1]>0){
+            mC.min=map[pos.Y][pos.X+1];
+            mC.co.X=pos.X+1;
+            mC.co.Y=pos.Y;
+        }
+    }
+    public static void checkDown(Coordinates pos, int[][] map, MinCoordinates mC){
+        if (map[pos.Y-1][pos.X] < mC.min && map[pos.Y-1][pos.X]>0){
+            mC.min=map[pos.Y-1][pos.X];
+            mC.co.X=pos.X;
+            mC.co.Y=pos.Y-1;
+        }
+    }
+    public static void checkLeft(Coordinates pos, int[][] map, MinCoordinates mC){
+        if (map[pos.Y][pos.X-1] < mC.min && map[pos.Y][pos.X-1]>0){
+            mC.min=map[pos.Y][pos.X-1];
+            mC.co.X=pos.X-1;
+            mC.co.Y=pos.Y;
+        }
+    }
+    public static Coordinates addEscape(int x,int y,int[][] map){
         map[y][x]=-2;
+        Coordinates posEsc = new Coordinates(x,y);
+        return posEsc;
     }
     public static void drawMap(int[][] map){
         for (int yi = 0; yi < map.length; yi+=1){
@@ -128,16 +196,6 @@ public class vave {
             }
             else map[y][x+i] = -1;
         }
-    }
-    
-    
+    }  
 }
 
-class Coordinates{
-    int X;
-    int Y;
-    Coordinates(int x, int y) {
-        this.X = x;
-        this.Y = y;
-    }
-}
