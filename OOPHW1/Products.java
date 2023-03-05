a200f8d5b9b6ad6df3e0c23a67a2ce362ad657a8
Farstron <109409135+Farstron@users.noreply.github.com>
2023-03-05 22:32:11 +0700

    add class Products

--- OOPHW1/Products.java
+++ OOPHW1/Products.java
@@ -1,3 +1,15 @@
 class Products {
-        
+        protected String name;
+        protected int price;
+        protected int count;
+        protected String unitOfMeasurement;
+        Products (String name, int price, int count, String unitOfMeasurement){
+            this.name = name;
+            this.price = price;
+            this.count = count;
+            this.unitOfMeasurement = unitOfMeasurement;
+        }
+        public String getInfo(){
+            return String.format("Наименование: %s;\nЦена: %d;\nКоличество: %d;\nЕдиница измерения: %s;", name, price, count, unitOfMeasurement);
+        }
 }
